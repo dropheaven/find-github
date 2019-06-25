8911da41ba288d60f40943dd0122ad6c344a3914
Saad Ahmed <dropheaven@live.com>
2019-06-24 22:49:38 -0400

    show found user's github page

--- javascript/app.js
+++ javascript/app.js
@@ -1,5 +1,7 @@
 // Initialize Github
 const github = new Github();
+// Initialize UI
+const ui = new UI();
 
 // Search input
 const searchUser = document.getElementById("search-user");
@@ -10,13 +12,14 @@ searchUser.addEventListener("keyup", e => {
   const userText = e.target.value;
 
   if (userText !== "") {
-    github.getUser(userText).then(data => {
-      if (data.profile.message === 'Not Found') {
-        // show alert
-      } else {
-        // show profile
-      }
-    });
+    github.getUser(userText)
+      .then(data => {
+        if (data.profile.message === 'Not Found') {
+          // show alert
+        } else {
+          ui.showProfile(data.profile);
+        }
+      });
   } else {
     // clear profile
   }
