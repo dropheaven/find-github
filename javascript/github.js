fff4413a225000b14dde6ff1348498df83603da2
Saad Ahmed <dropheaven@live.com>
2019-06-26 17:42:51 -0400

    moved client id and secret to separate file

--- javascript/github.js
+++ javascript/github.js
@@ -1,12 +1,11 @@
 class Github {
   constructor() {
-    this.clientID = "dce3626eef855d688adf";
-    this.clientSecret = "f93b5fae6fa37c1627d1ce3ea5636f3e7613ff31";
+
   }
 
   async getUser(user) {
     const profileResponse = await fetch(
-      `https://api.github.com/users/${user}?client_id=${this.clientID}&clientSecret=${this.clientSecret}`
+      `https://api.github.com/users/${user}?client_id=${clientID}&clientSecret=${clientSecret}`
     );
 
     const profile = await profileResponse.json();
