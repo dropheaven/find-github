4f79470339d5133e14cbe2cc7f4197f006d9c0ce
Saad Ahmed <dropheaven@live.com>
2019-06-28 23:36:15 -0400

    list 5 most recent repos

--- javascript/github.js
+++ javascript/github.js
@@ -8,10 +8,16 @@ class Github {
       `https://api.github.com/users/${user}?client_id=${clientID}&client_secret=${clientSecret}`
     );
 
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=5&sort=created:asc&client_id=${clientID}&client_secret=${clientSecret}`
+    );
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
-      profile
+      profile,
+      repos
     };
   }
 }
